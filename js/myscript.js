eb50709280763c15b8db8daa0239b7e6da42ddbd
Emilo <emilio.furnari87@gmail.com>
2020-11-25 17:03:51 +0100

    js non va

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,16 @@
+$(document).ready(function () {
 
+  // IMPOSTO IL MOUSEHOVER SU SUBSCRIPTION
+  $("#subscription").mouseover(
+    function () {
+      // RIMUOVO CLASSE INACTIVE(DISPLAY NONE)
+      $("#dropdown_subscription").removeClass("inactive");
+    }
+  );
+  $("#subscription").mouseout(
+    function () {
+      // AGGIUNGO CLASSE ACTIVE(DISPLAY BLOCK)
+      $("#dropdown_subscription").addClass("active");
+    }
+  );
+});
